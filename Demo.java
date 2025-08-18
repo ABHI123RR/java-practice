4e790bb3ef10976786dc55ef1f9b754d7a72c77e
ABHI123RR <abhirocks.benachigere2005@gmail.com>
2025-08-18 18:01:02 +0530

    init

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+
+    public static void main(String a[])
+    {
+        int nums[][] = new int[3][4];  // Corrected "in" to "int"
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                System.out.println(nums[i][j]);
+            }
+        }
+    }
+}
