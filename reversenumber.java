d6155d8d8ab2b27d5981062cd0df19ae0cca1861
ABHI123RR <abhirocks.benachigere2005@gmail.com>
2025-08-19 19:27:26 +0530

    init

--- reversenumber.java
+++ reversenumber.java
@@ -1,6 +1,6 @@
 public class reversenumber {
     public static void main(String[] args) {
-        for(int i = 10; i>=1; i--){
+        for(int i = 10; i<=1; i--){
             System.out.println(i);
         }
     }
