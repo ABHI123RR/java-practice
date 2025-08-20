fc17612a96a12bb5c48ebac54f8fa38ee17cd71e
ABHI123RR <abhirocks.benachigere2005@gmail.com>
2025-08-20 22:24:34 +0530

    init

---
+++ Palindromecheck.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Palindromecheck {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string or number");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+
+        for (int i = input.length() -1; i >=0; i--) {
+            reversed += input.charAt(i);
+
+        }
+        if (input.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+         }
+         scanner.close();
+        }
+    }
+    
+
